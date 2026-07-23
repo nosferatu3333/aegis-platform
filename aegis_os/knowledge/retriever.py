0935257a57ff5b3cc53d67e3537ab373207b19d2
Woolis Shop <mailforcanva2222@gmail.com>
2026-07-23 17:54:08 -0500

    Upgrade knowledge retriever with contextual search

--- aegis_os/knowledge/retriever.py
+++ aegis_os/knowledge/retriever.py
@@ -1,17 +1,60 @@
 class Retriever:
     """
     Retrieves relevant knowledge
-    from the knowledge base.
+    from the knowledge system.
     """
 
     def __init__(self, knowledge_base):
+
         self.knowledge_base = knowledge_base
 
 
-    def search(self, concept):
+    def search(self, query):
 
-        result = self.knowledge_base.get(
-            concept
+        results = []
+
+
+        knowledge_items = (
+            self.knowledge_base.all()
         )
 
-        return result
+
+        query_words = set(
+            query.lower().split()
+        )
+
+
+        for item in knowledge_items:
+
+            text = str(item).lower()
+
+
+            item_words = set(
+                text.split()
+            )
+
+
+            similarity = len(
+                query_words.intersection(
+                    item_words
+                )
+            )
+
+
+            if similarity > 0:
+
+                results.append(
+                    {
+                        "knowledge": item,
+                        "relevance": similarity
+                    }
+                )
+
+
+        results.sort(
+            key=lambda x: x["relevance"],
+            reverse=True
+        )
+
+
+        return results
